40c07294aa2d7593baeb40ee7130b77897540763
Giancarlo <gianco92@hotmail.it>
2017-10-14 18:45:23 +0200

    New handler

--- node-scripts/handler.js
+++ node-scripts/handler.js
@@ -4,21 +4,25 @@ var chokidar = require('chokidar');
 var watcher = chokidar.watch('./Commitments', {ignoreInitial: true});
 
 var config = require('../config/settings');
-var zmq = require('zeromq'),
-	sock = zmq.socket('pub');
+//var zmq = require('zeromq');
+//var sock = zmq.socket('pub');
 
 var host = config.sensor.address.host;
 var port = config.sensor.address.port;
 
-sock.bindSync('tcp://' + host + ':' + port);
+//sock.bindSync('tcp://' + host + ':' + port);
 
 //var TempPublisher = require('./TempPublisher');
 var TiltPublisher = require('./TiltPublisher');
 var TempHumPublisher = require('./TempHumPublisher');
 
 //var tempSensor = new TempPublisher(sock);
-var tiltSensor = new TiltPublisher(sock);
-var tempHumSensor = new TempHumPublisher(sock);
+//var tiltSensor = new TiltPublisher(sock);
+//var tempHumSensor = new TempHumPublisher(sock);
+
+var temp_line = [];
+var tilt_line = [];
+var temp_hum_line = [];
 
 class Handler {
 	initializeHandler() {
@@ -29,7 +33,35 @@ class Handler {
 			var file = JSON.parse(fs.readFileSync('./' + path, 'utf8'));
 			console.log('\nNew File : ' + path);
 			var child = fork.fork('./chandler.js');
-			
+			if (temp_line.length == 0)
+			{
+				if (file.commitment.antecedentCondition.operation == '&&' || file.commitment.antecedentCondition.operation == '||' ||
+					file.commitment.consequentCondition.operation == '&&' || file.commitment.consequentCondition.operation == '&&')
+				{
+					if (file.commitment.antecedentCondition.variables[0].variables[0] == 'temp' ||
+						file.commitment.antecedentCondition.variables[1].variables[0] == 'temp' ||
+							file.commitment.consequentCondition.variables[0].variables[0] == 'temp' ||
+								file.commitment.consequentCondition.variables[1].variables[0] == 'temp')
+					{
+						//tempSensor.sense();
+						temp_line.push(child.pid);
+						console.log("Connected to Temp Sensor\n");
+					}
+				}
+				else if (file.commitment.antecedentCondition.operation == '==' || file.commitment.antecedentCondition.operation == '!=' ||
+						file.commitment.consequentCondition.operation == '==' || file.commitment.consequentCondition.operation == '!=' ||
+							file.commitment.antecedentCondition.operation == '<' || file.commitment.antecedentCondition.operation == '>' ||
+								file.commitment.consequentCondition.operation == '<' || file.commitment.consequentCondition.operation == '>')
+				{
+					if (file.commitment.antecedentCondition.variables[0] == 'temp' ||
+						file.commitment.consequentCondition.variables[0] == 'temp')
+					{
+						//tempSensor.sense();
+						temp_line.push(child.pid);
+						console.log("Connected to Temp Sensor\n");
+					}
+				}
+			}
 			//DA SPOSTARE DOPO IL CONTROLLO
 			//tempSensor.sense();
 			//tiltSensor.sense();
